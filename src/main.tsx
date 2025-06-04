36a6ec2265d07efb768bef5349367071775ea607
Draho <drahoslav@yo2.cz>
2025-06-04 14:58:10 +0200

    feat: add Sentry error tracking and remove localStorage clearing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
 
-window.addEventListener('error', () => {
-  localStorage.clear()
-  console.warn('Clearing localStorage')
-  // setTimeout(() => {
-  //   window.location.reload()
-  // }, 30000)
+// Initialize Sentry
+Sentry.init({
+  dsn: 'https://0f9f9cd80b94ff809d3bbb1da37fb869@o315999.ingest.us.sentry.io/4509440506658816',
+  // Setting this option to true will send default PII data to Sentry
+  sendDefaultPii: true,
 })
 
 createRoot(document.getElementById('root')!).render(
